41e04f1a895e047938b05975929bf7e1da1cbcb6
Yuan (Terry) Tang <terrytangyuan@gmail.com>
2023-12-02 14:10:59 -0500

    Update setup_machine.sh

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Install Softwares
-bash install/all.sh
-
 # Copy Configurations and helper scripts
-cp atom/* ~/.atom/
 sudo mkdir /usr/local/bin
 sudo cp bin/* /usr/local/bin/
 cp git/.gitconfig ~/
+
+# Install Softwares
+bash install/all.sh
